04678eab53449d06db9263f8844e3cb11a49e494
DiegoCas0807 <dch.ingeniero@gmail.com>
2019-04-27 13:32:16 +0200

    Abstracción lógica de CLI por CLIProtocol class

--- pokeApi/src/main/java/tv/codely/java_bootstrap/PokemonClient.java
+++ pokeApi/src/main/java/tv/codely/java_bootstrap/PokemonClient.java
@@ -1,8 +1,5 @@
 package tv.codely.java_bootstrap;
 
-import me.sargunvohra.lib.pokekotlin.client.PokeApi;
-import me.sargunvohra.lib.pokekotlin.client.PokeApiClient;
-import me.sargunvohra.lib.pokekotlin.model.Pokemon;
 import me.sargunvohra.lib.pokekotlin.model.PokemonType;
 
 import java.util.List;
@@ -12,9 +9,8 @@ public class PokemonClient {
     String pokename = "Charmander";
 
     public static void main(String[] args) {
-        PokeApi pokeApi = new PokeApiClient();
-        Pokemon pokemon = pokeApi.getPokemon(1);
-        final List<PokemonType> types = pokemon.getTypes();
+        CLIProtocol clip = new CLIProtocol();
+        List<PokemonType> types = clip.getPokeTypeById(1);
         types.forEach((p) -> {System.out.println(p.getType().getName());});
     }
 
