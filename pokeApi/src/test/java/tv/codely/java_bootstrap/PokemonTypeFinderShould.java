6af695138bbb1d06e4f80ae754cbd270eba36a68
joancliment <joancliment.palou@students.salle.url.edu>
2019-04-30 21:40:12 +0200

    se añaden test unitarios de pokemon correcto y pokemon no encontrado

---
+++ pokeApi/src/test/java/tv/codely/java_bootstrap/PokemonTypeFinderShould.java
@@ -0,0 +1,38 @@
+package tv.codely.java_bootstrap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PokemonTypeFinderShould {
+
+	@Test
+	void pokemon_pikachu() throws IOException, PokeTypeException{
+		String pokemonTeclado = "pikachu";
+
+		PokemonTypeFinder pokeFinder = new PokemonTypeFinder();
+        final List<PokeType> pokeTypes = pokeFinder.pokeFind(pokemonTeclado);
+
+		final String actualPokemonTypeName = pokeTypes.get(0).getName();
+		final String expectedPokemonTypeName = "electric";
+
+		assertEquals(expectedPokemonTypeName, actualPokemonTypeName);
+	}
+
+	@Test
+	void pokemon_not_found() throws IOException, PokeTypeException{
+		String pokemonTeclado = "";
+
+		PokemonTypeFinder pokeFinder = new PokemonTypeFinder();
+		final List<PokeType> pokeTypes = pokeFinder.pokeFind(pokemonTeclado);
+
+		final String actualPokemonTypeName = pokeTypes.get(0).getName();
+		final String expectedPokemonTypeName = "electric";
+
+		assertEquals(expectedPokemonTypeName, actualPokemonTypeName);
+	}
+
+}
