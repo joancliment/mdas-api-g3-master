4e277d8e44676819353e374ba8d2a0a126c0ec76
joancliment <joancliment.palou@students.salle.url.edu>
2019-05-01 16:38:05 +0200

    terminado el test que controla la excepción

--- pokeApi/src/test/java/tv/codely/java_bootstrap/PokemonTypeFinderShould.java
+++ pokeApi/src/test/java/tv/codely/java_bootstrap/PokemonTypeFinderShould.java
@@ -1,16 +1,21 @@
 package tv.codely.java_bootstrap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
 
+
+
 public class PokemonTypeFinderShould {
 
+
 	@Test
-	void pokemon_pikachu() throws IOException, PokeTypeException{
+	public void pokemon_found() throws IOException, PokeTypeException{
 		String pokemonTeclado = "pikachu";
 
 		PokemonTypeFinder pokeFinder = new PokemonTypeFinder();
@@ -23,16 +28,18 @@ public class PokemonTypeFinderShould {
 	}
 
 	@Test
-	void pokemon_not_found() throws IOException, PokeTypeException{
+	public void testPokeTypeException() throws IOException, PokeTypeException {
 		String pokemonTeclado = "";
 
 		PokemonTypeFinder pokeFinder = new PokemonTypeFinder();
-		final List<PokeType> pokeTypes = pokeFinder.Find(pokemonTeclado);
 
-		final String actualPokemonTypeName = pokeTypes.get(0).getName();
-		final String expectedPokemonTypeName = "Pokemon no encontrado";
 
-		assertEquals(expectedPokemonTypeName, actualPokemonTypeName);
+		final String expectedPokemonTypeException = "Pokemon no encontrado";
+
+		Exception exception = assertThrows(PokeTypeException.class, () ->
+				pokeFinder.Find(pokemonTeclado));
+		assertEquals(expectedPokemonTypeException, exception.getMessage());
+
 	}
 
 }
